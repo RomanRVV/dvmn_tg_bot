08d77f3fed8ebb9902f245d5544516cdc489e3a8
RomanRVV <rvachyov.roma@yandex.ru>
2023-09-11 19:56:29 +0300

    fix problems

--- bot.py
+++ bot.py
@@ -1,60 +1,78 @@
 import argparse
 import time
+import textwrap as tw
 
 import requests
 import telegram
 from environs import Env
 
-env = Env()
-env.read_env()
 
-tg_token = env('TG_TOKEN')
-dvmn_token = env('DVMN_TOKEN')
+def main():
+    env = Env()
+    env.read_env()
 
-parser = argparse.ArgumentParser(description='Скрипт отправляет уведомление о проверке работ в указанный тг чат')
-parser.add_argument('chat_id',
-                    help='укажите id своего телеграмм чата')
-args = parser.parse_args()
+    tg_token = env('TG_TOKEN')
+    dvmn_token = env('DVMN_TOKEN')
 
-bot = telegram.Bot(token=tg_token)
-chat_id = args.chat_id
+    parser = argparse.ArgumentParser(
+        description='Скрипт отправляет уведомление о проверке работ в указанный тг чат')
+    parser.add_argument('chat_id',
+                        help='укажите id своего телеграмм чата')
+    args = parser.parse_args()
 
-payload = {}
+    bot = telegram.Bot(token=tg_token)
+    chat_id = args.chat_id
 
-while True:
+    timestamp = None
 
-    url = 'https://dvmn.org/api/long_polling/'
+    while True:
+        url = 'https://dvmn.org/api/long_polling/'
 
-    headers = {
-        "Authorization": f'Token {dvmn_token}'
-    }
-    try:
-        raw_response = requests.get(url, headers=headers, params=payload, timeout=5)
-        raw_response.raise_for_status()
-    except requests.exceptions.ReadTimeout:
-        continue
-    except requests.exceptions.ConnectionError:
-        print('Ошибка соединения сети')
-        time.sleep(10)
-        continue
+        headers = {
+            "Authorization": f'Token {dvmn_token}'
+        }
+        try:
+            payload = {
+                'timestamp': timestamp
+            }
+            raw_response = requests.get(
+                url,
+                headers=headers,
+                params=payload,
+                timeout=5)
+            raw_response.raise_for_status()
+        except requests.exceptions.ReadTimeout:
+            continue
+        except requests.exceptions.ConnectionError:
+            print('Ошибка соединения сети')
+            time.sleep(10)
+            continue
 
-    response = raw_response.json()
-    payload = {
-        'timestamp': response.get('timestamp_to_request')
-    }
+        dvmn_response = raw_response.json()
 
-    if response['status'] == 'timeout':
-        continue
+        if dvmn_response['status'] == 'timeout':
+            timestamp = dvmn_response.get('timestamp_to_request')
+            continue
+        timestamp = dvmn_response.get('last_attempt_timestamp')
 
-    new_attempts = response['new_attempts']
-    for attempt in new_attempts:
-        if attempt['is_negative']:
-            bot.send_message(chat_id=chat_id,
-                             text=f'''У вас проверили работу "{attempt['lesson_title']}"\n
-Ссылка на урок - {attempt['lesson_url']}\n
-К сожалению в работе нашлись ошибки''')
-        else:
-            bot.send_message(chat_id=chat_id,
-                             text=f'''У вас проверили работу "{attempt['lesson_title']}"\n
-Ссылка на урок - {attempt['lesson_url']}\n
-Преподавателю все понравилось, можно приступать к следующему уроку!''')
+        new_attempts = dvmn_response['new_attempts']
+        for attempt in new_attempts:
+            if attempt['is_negative']:
+                bot.send_message(chat_id=chat_id,
+                                 text=tw.dedent(f'''
+                                 У вас проверили работу "{attempt['lesson_title']}"\n
+                                 Ссылка на урок - {attempt['lesson_url']}\n
+                                 К сожалению в работе нашлись ошибки
+                                    '''))
+            else:
+                bot.send_message(chat_id=chat_id,
+                                 text=tw.dedent(f'''
+                                 У вас проверили работу "{attempt['lesson_title']}"\n
+                                 Ссылка на урок - {attempt['lesson_url']}\n
+                                 Преподавателю все понравилось, можно приступать 
+                                 к следующему уроку!
+                                    '''))
+
+
+if __name__ == '__main__':
+    main()
