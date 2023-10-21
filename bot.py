0cb8cf0f015e9f5c0ddad592b7bf4b85e2b28350
RomanRVV <rvachyov.roma@yandex.ru>
2023-10-21 19:30:07 +0300

    send logs to Telegram to admin user

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import argparse
 import time
 import textwrap as tw
+import logging
 
 import requests
 import telegram
@@ -23,8 +24,15 @@ def main():
     bot = telegram.Bot(token=tg_token)
     chat_id = args.chat_id
 
-    timestamp = None
+    class MyLogsHandler(logging.Handler):
+        def emit(self, record):
+            log_message = self.format(record)
+            bot.send_message(chat_id=chat_id, text=log_message)
 
+    bot.logger.addHandler(MyLogsHandler())
+    bot.logger.warning('Бот запущен')
+
+    timestamp = None
     while True:
         url = 'https://dvmn.org/api/long_polling/'
 
@@ -41,37 +49,41 @@ def main():
                 params=payload,
                 timeout=5)
             raw_response.raise_for_status()
+
+            dvmn_review_info = raw_response.json()
+
+            if dvmn_review_info['status'] == 'timeout':
+                timestamp = dvmn_review_info.get('timestamp_to_request')
+                continue
+            timestamp = dvmn_review_info.get('last_attempt_timestamp')
+
+            new_attempts = dvmn_review_info['new_attempts']
+            for attempt in new_attempts:
+                if attempt['is_negative']:
+                    bot.send_message(chat_id=chat_id,
+                                     text=tw.dedent(f'''
+                                             У вас проверили работу "{attempt['lesson_title']}"\n
+                                             Ссылка на урок - {attempt['lesson_url']}\n
+                                             К сожалению в работе нашлись ошибки
+                                                '''))
+                else:
+                    bot.send_message(chat_id=chat_id,
+                                     text=tw.dedent(f'''
+                                             У вас проверили работу "{attempt['lesson_title']}"\n
+                                             Ссылка на урок - {attempt['lesson_url']}\n
+                                             Преподавателю все понравилось, можно приступать 
+                                             к следующему уроку!
+                                                '''))
+
         except requests.exceptions.ReadTimeout:
             continue
         except requests.exceptions.ConnectionError:
             print('Ошибка соединения сети')
             time.sleep(10)
             continue
-
-        dvmn_review_info = raw_response.json()
-
-        if dvmn_review_info['status'] == 'timeout':
-            timestamp = dvmn_review_info.get('timestamp_to_request')
-            continue
-        timestamp = dvmn_review_info.get('last_attempt_timestamp')
-
-        new_attempts = dvmn_review_info['new_attempts']
-        for attempt in new_attempts:
-            if attempt['is_negative']:
-                bot.send_message(chat_id=chat_id,
-                                 text=tw.dedent(f'''
-                                 У вас проверили работу "{attempt['lesson_title']}"\n
-                                 Ссылка на урок - {attempt['lesson_url']}\n
-                                 К сожалению в работе нашлись ошибки
-                                    '''))
-            else:
-                bot.send_message(chat_id=chat_id,
-                                 text=tw.dedent(f'''
-                                 У вас проверили работу "{attempt['lesson_title']}"\n
-                                 Ссылка на урок - {attempt['lesson_url']}\n
-                                 Преподавателю все понравилось, можно приступать 
-                                 к следующему уроку!
-                                    '''))
+        except Exception as e:
+            error_message = f"Бот упал с ошибкой: {str(e)}"
+            bot.logger.warning(error_message)
 
 
 if __name__ == '__main__':
